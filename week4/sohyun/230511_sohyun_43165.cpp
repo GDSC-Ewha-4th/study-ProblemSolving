529e40114fe47b586c7ccd0a0aa80698c0fbdaa6
sohyundoh <lyny.doh@gmail.com>
2023-05-11 09:08:49 +0900

    Create 230511_sohyun_43165.cpp

---
+++ week4/sohyun/230511_sohyun_43165.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+    queue<pair<int, int>> q;
+    int answer = 0;
+    q.push({0, 0});
+    while (!q.empty()) {
+        int sum = q.front().first;
+        int index = q.front().second;
+        q.pop();
+        if (index < numbers.size()) {
+            int plus = sum + numbers[index];
+            int minus = sum - numbers[index];
+            if (index == numbers.size() - 1 && (plus == target || minus == target)) {
+                answer++;
+                continue;
+            }
+            q.push({plus, index + 1});
+            q.push({minus, index + 1});
+        }
+        //while문은 되게 여러번 돌기 때문에 while문의 반복횟수 만큼 index를 늘리면 안된다.
+    }
+    return answer;
+}
