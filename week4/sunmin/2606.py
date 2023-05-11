7fab68a41a54e923a12f41dbd997c4a2a60692de
Sunmin Lee <79503414+sunmin-lee99@users.noreply.github.com>
2023-05-11 22:51:06 +0900

    Update 2606.py

--- week4/sunmin/2606.py
+++ week4/sunmin/2606.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # 컴퓨터의 수
 v = int(input())
 
@@ -6,18 +8,26 @@ e = int(input())
 graph = [[] for _ in range(v+1)]
 for _ in range(e):
     a, b = map(int, input().split())
+    
     # 연결된 컴퓨터의 정보가 언제가 1부터 등장한다는 보장 x
     graph[a].append(b)
     graph[b].append(a)
 
 
-# 재귀적 구현
-def dfs(x, count):
+# 큐 구현
+def bfs(x):
+    deq = deque([x])
+    count = 0
     visited[x] = True
-    for node in graph[x]:
-        if not visited[node]:
-            count = dfs(node, count+1)
+    while deq:
+        node = deq.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                deq.append(next_node)
+                count += 1
+
     return count
 
 visited = [False for _ in range(v+1)]
-print(dfs(1, 0))
+print(bfs(1))
