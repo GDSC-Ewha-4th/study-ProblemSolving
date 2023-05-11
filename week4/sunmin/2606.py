bd2cb22e02a2bbb3ee421d6a27ac1347ea970104
Sunmin Lee <79503414+sunmin-lee99@users.noreply.github.com>
2023-05-11 22:28:04 +0900

    Update 2606.py

--- week4/sunmin/2606.py
+++ week4/sunmin/2606.py
@@ -6,26 +6,18 @@ e = int(input())
 graph = [[] for _ in range(v+1)]
 for _ in range(e):
     a, b = map(int, input().split())
-    
     # 연결된 컴퓨터의 정보가 언제가 1부터 등장한다는 보장 x
     graph[a].append(b)
     graph[b].append(a)
 
 
-# 스택 구현
-def dfs(x):
-    stack = [x]
+# 재귀적 구현
+def dfs(x, count):
     visited[x] = True
-    count = 0
-    while stack:
-        node = stack.pop()
-        for next_node in graph[node]:
-            if not visited[next_node]:
-                visited[next_node] = True
-                stack.append(next_node)
-                count += 1
+    for node in graph[x]:
+        if not visited[node]:
+            count = dfs(node, count+1)
     return count
 
 visited = [False for _ in range(v+1)]
-
-print(dfs(1))
+print(dfs(1, 0))
