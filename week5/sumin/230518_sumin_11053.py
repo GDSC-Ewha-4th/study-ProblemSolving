196124bf787b7995468e09f8d4a2369338a68b10
Sumin Ha <minha62@ewhain.net>
2023-05-18 18:44:25 +0900

    [BOJ] solove 11053

--- week5/sumin/230518_sumin_11053.py
+++ week5/sumin/230518_sumin_11053.py
@@ -1,12 +1,11 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-dp = [0 for _ in range(n)]
+dp = [1 for _ in range(n)]
 
-for i in range(n):
-    for j in range(n):
-        if a[j] < a[i] and dp[j] > dp[i]:
-            dp[i] = dp[j]
-    dp[i] += 1 
+for i in range(1, n):
+    for j in range(i):
+        if a[i] > a[j]:
+        	dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
